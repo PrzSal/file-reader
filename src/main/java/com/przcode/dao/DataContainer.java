baad89b60a7f594214678c43f3b733a5b651cdd0
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 16:40:27 +0100

    add method add()

--- src/main/java/com/przcode/dao/DataContainer.java
+++ src/main/java/com/przcode/dao/DataContainer.java
@@ -5,12 +5,22 @@ import com.przcode.model.Data;
 import com.przcode.model.Id;
 import com.przcode.model.Pair;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class DataContainer<K, V> {
     private Collection<Pair<Id, Data>> listData;
-    
-    public void load(DataProvider<Pair<K, V>> dataProvider){
+
+    public DataContainer() {
+        this.listData = new ArrayList<>();
+    }
+
+    public void load(DataProvider<Pair<K, V>> dataProvider) {
         this.listData = dataProvider.getAll();
+    }
+
+    public void add(Id id, Data data) {
+        Pair<Id, Data> pair = new Pair<>(id, data);
+        listData.add(pair);
     }
 }
