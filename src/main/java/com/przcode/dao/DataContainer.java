ddef4e3b6471d3001e6664fb0710122dc4043827
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 19:57:22 +0100

    add method update()

--- src/main/java/com/przcode/dao/DataContainer.java
+++ src/main/java/com/przcode/dao/DataContainer.java
@@ -1,12 +1,11 @@
 package com.przcode.dao;
 
 import com.przcode.controller.DataProvider;
-import com.przcode.model.Data;
-import com.przcode.model.Id;
 import com.przcode.model.Pair;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class DataContainer<ID, DATA> {
     private Collection<Pair<ID, DATA>> listData;
@@ -22,5 +21,17 @@ public class DataContainer<ID, DATA> {
     public void add(ID id, DATA data) {
         Pair<ID, DATA> pair = new Pair<>(id, data);
         listData.add(pair);
+    }
+
+    public void update(ID id,DATA data) {
+
+        listData.stream()
+                .filter(pair -> pair.getId().equals(id))
+                .peek(pair -> pair.setData(data))
+                .collect(Collectors.toList());
+    }
+
+    public Collection<Pair<ID, DATA>> getListData() {
+        return listData;
     }
 }
