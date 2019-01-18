1474bf5394c7d1ddd5cc01a3feda6014689665df
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 22:43:27 +0100

    add method toString()

--- src/main/java/com/przcode/model/Data.java
+++ src/main/java/com/przcode/model/Data.java
@@ -1,9 +1,22 @@
 package com.przcode.model;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Data {
     private String[] data;
 
     public Data(String[] data) {
         this.data = data;
+    }
+
+    public String[] getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(data)
+                .collect(Collectors.joining());
     }
 }
