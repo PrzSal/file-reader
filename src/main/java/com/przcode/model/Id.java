89fc97ebd6cb94d60e7743be7fc9ae1e4e3c2d21
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 20:01:38 +0100

    add model Id and equals and hashCode methods

--- src/main/java/com/przcode/model/Id.java
+++ src/main/java/com/przcode/model/Id.java
@@ -1,4 +1,24 @@
 package com.przcode.model;
 
+import java.util.Objects;
+
 public class Id {
+    private Long id;
+
+    public Id(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Id id1 = (Id) o;
+        return Objects.equals(id, id1.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
