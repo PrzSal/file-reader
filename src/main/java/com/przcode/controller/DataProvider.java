1dd1aefa84555c80f1d633b537c566f51fda528c
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 11:39:11 +0100

    add constructor and two fields nameFile, listContent

--- src/main/java/com/przcode/controller/DataProvider.java
+++ src/main/java/com/przcode/controller/DataProvider.java
@@ -1,13 +1,21 @@
 package com.przcode.controller;
 
-import com.przcode.model.Pair;
-
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class DataProvider<T> {
+    private String nameFile;
+    private ArrayList<String[]> listContent;
+
+    public DataProvider(String nameFile) {
+        this.listContent = new ArrayList<>();
+        this.nameFile = nameFile;
+    }
+
     private int count(){
         return 0;
     }
+
     public Collection<T> get(int page, int pageLength) {
         return null;
     }
