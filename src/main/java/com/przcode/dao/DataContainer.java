ab3b0167627cd83c53a9f89016a597a8c33097a2
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-18 16:23:40 +0100

    add method load()

--- src/main/java/com/przcode/dao/DataContainer.java
+++ src/main/java/com/przcode/dao/DataContainer.java
@@ -1,4 +1,16 @@
 package com.przcode.dao;
 
+import com.przcode.controller.DataProvider;
+import com.przcode.model.Data;
+import com.przcode.model.Id;
+import com.przcode.model.Pair;
+
+import java.util.Collection;
+
 public class DataContainer<K, V> {
+    private Collection<Pair<Id, Data>> listData;
+    
+    public void load(DataProvider<Pair<K, V>> dataProvider){
+        this.listData = dataProvider.getAll();
+    }
 }
