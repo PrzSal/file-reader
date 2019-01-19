8756abaf6778972c5042e4dda70f2496e98944db
Przemo8922 <przemyslawsalak@gmail.com>
2019-01-19 14:34:35 +0100

    remove appTest

--- src/main/java/com/przcode/App.java
+++ src/main/java/com/przcode/App.java
@@ -1,6 +1,7 @@
 package com.przcode;
 
-import com.przcode.controller.DataProvider;
+import com.przcode.controller.DataProviderImpl;
+import com.przcode.dao.DataContainer;
 import com.przcode.model.Data;
 import com.przcode.model.Id;
 import com.przcode.model.Pair;
@@ -8,7 +9,19 @@ import com.przcode.model.Pair;
 public class App 
 {
     public static void main( String[] args ){
-        DataProvider<Pair<Id, Data>> dC = new DataProvider<>(args[0]);
+        DataProviderImpl<Pair<Id, Data>> dP = new DataProviderImpl<>(args[0]);
 //        dC.readFile();
+        DataContainer<Id, Data> dC = new DataContainer<>();
+//
+        String[]h={"Pol","War"};
+//        dC.add(new Id(new Long(5)), new Data(h));
+//        System.out.println(dC.getListData().size());
+//        dC.add(new Id(new Long(4)), new Data(h));
+        dC.load(dP);
+//        Data d = dC.findById(new Id(new Long(3)));
+//        System.out.println(d.getData()[1]);
+        //        dC.update(new Id(new Long(1)), new Data(h));
+//
+        dC.print(0,1);
     }
 }
